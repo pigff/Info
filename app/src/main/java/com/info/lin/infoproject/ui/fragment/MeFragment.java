065ec80661b100498e417dac94586288e4a2a99c
pigff <androiddevzf@163.com>
2017-02-21 10:21:07 +0800

    test

--- app/src/main/java/com/info/lin/infoproject/ui/fragment/MeFragment.java
+++ app/src/main/java/com/info/lin/infoproject/ui/fragment/MeFragment.java
@@ -9,14 +9,9 @@ import android.view.ViewGroup;
 
 import com.info.lin.infoproject.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class MeFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -29,15 +24,6 @@ public class MeFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MeFragment newInstance(String param1, String param2) {
         MeFragment fragment = new MeFragment();
         Bundle args = new Bundle();
